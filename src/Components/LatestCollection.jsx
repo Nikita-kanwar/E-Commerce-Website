323d4248c194c8c6a42d825b4d8aa8a78da6ce22
Nikita Kanwar <kanwarnikita2025@gmail.com>
2025-02-19 12:54:03 +0530

    Products

--- src/Components/LatestCollection.jsx
+++ src/Components/LatestCollection.jsx
@@ -1,13 +1,16 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../Context/ShopContext'
 import Title from './Title';
+import ProductItem from './ProductItem';
 
 const LatestCollection = () => {
 
      const { products } = useContext(ShopContext);
+     const [latestProducts,setLatestProducts]  = useState([]);
 
-    
-     
+     useEffect (()=>{
+            setLatestProducts(products.slice(0,10));
+     },[])       
 
   return (
     <div className='my-10'>
@@ -17,7 +20,13 @@ const LatestCollection = () => {
                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia iure fugiat laborum perspiciatis? Quibusdam, laudantium?
           </p>
        </div>
-       
+       <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+           {
+            latestProducts.map((item,index)=>(
+               <ProductItem key={index} id={item._id} image={item.image} name={item.nam}  price={item.price} />
+            ))
+           }
+       </div>
     </div>
   )
 }
