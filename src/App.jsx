e833524f39fe39dfac4eb147aa75fc5b9cbfc3b0
Nikita Kanwar <kanwarnikita2025@gmail.com>
2025-02-23 20:44:51 +0530

    Create Footer UI

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Login from './Pages/Login'
 import PlaceOrder from './Pages/PlaceOrder'
 import Orders from './Pages/Orders'
 import Navbar from './Components/Navbar'
+import Footer from './Components/Footer'
 
 
 const App = () => {
@@ -29,6 +30,7 @@ const App = () => {
       <Route path='/orders' element={<Orders/>} />
 
       </Routes>
+      <Footer/>
     
     </div>
   )
