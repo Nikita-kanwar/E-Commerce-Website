262d6d551d10f218e15e31ea596c1d8463c5c3d2
Nikita Kanwar <kanwarnikita2025@gmail.com>
2025-03-05 22:41:24 +0530

    Why choose us Details

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -13,8 +13,29 @@ const About = () => {
   <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, corporis explicabo quae exercitationem rem atque ad eum praesentium harum natus?</p>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit reiciendis, et dolorem quia nulla veritatis.</p>
+            <b className='text-gray-800' >Our Mission</b>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio error veritatis maiores, quaerat saepe perspiciatis aliquam maxime fugiat qui veniam!</p>
   </div>
   </div>  
+  <div className='text-2xl py-4'>
+    <Title text1={'Why'} text2={'Choose Us'} />
+
+  </div>
+  <div className='flex flex-col md:flex-row text-sm mb-20'>
+    <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+              <b>Quality Assurance:</b>
+              <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, aperiam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sunt autem ipsam amet accusamus labore distinctio tenetur dolorem quos architecto.</p>
+    </div>
+    <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+              <b>Convenience:</b>
+              <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, aperiam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sunt autem ipsam amet accusamus labore distinctio tenetur dolorem quos architecto.</p>
+    </div>
+    <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+              <b>Exceptional Customer Service:</b>
+              <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, aperiam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sunt autem ipsam amet accusamus labore distinctio tenetur dolorem quos architecto.</p>
+    </div>
+
+  </div>
     </div>
   )
 }
