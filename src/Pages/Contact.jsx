d1e1f63e54e822038275e5d86cf069f6c22e4ee8
Nikita Kanwar <kanwarnikita2025@gmail.com>
2025-03-05 23:00:17 +0530

    Create Contact Page

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import Title from '../Components/Title'
+import {assets} from '../assets/assets'
+import NewsLetterBox from '../Components/NewsLetterBox'
 
 const Contact = () => {
   return (
     <div>
-        
-    </div>
+        <div className='text-center text-2xl pt-10 border-t'>
+          <Title text1={'Contact'} text2={'Us'} />
+          </div>
+               <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28'>
+                    < img className='w-full md:max-w-[480px]' src={assets.contact_img} alt="" />
+                    <div className='flex flex-col justify-center items-start gap-6'>
+                      <p className='font-semibold text-xl text-gray-600'>Our Store</p>
+                      <p className='text-gray-500'>078- Sector24,Jaipur <br /> Rajasthan, India</p>
+                      <p className='text-gray-500'>Tel: +91 87865358t7703 <br /> Email:nkestore@gmail.com</p>
+                      <p className='font-semibold text-xl text-gray-600'> Careers ate Store</p>
+                      <p className='text-gray-500'>Learn more about us </p>
+
+                      <button className='border border-black px-8 text-sm hover:bg-black hover:text-white transition-all duration-500 cursor-pointer'>Explore Jobs</button>
+
+                    </div>
+               </div>
+               <NewsLetterBox />
+        </div>
+    
   )
 }
 
