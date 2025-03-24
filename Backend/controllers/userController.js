897e6ba1afcd76f12d7ee3adefda4c4e489aabe4
Nikita Kanwar <kanwarnikita2025@gmail.com>
2025-03-24 10:13:27 +0530

    Create controllers

---
+++ Backend/controllers/userController.js
@@ -0,0 +1,18 @@
+
+// Route for use login
+const loginUser = async (req,res) => {
+
+
+}
+
+// Route for user registeration
+const registerUser = async (req,res) =>{
+
+}
+
+// Route for admin login
+const adminLogin = async (req,res) =>{
+
+}
+
+export {loginUser,registerUser,adminLogin}
