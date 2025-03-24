381de96b82335e5343191da1f7394dfa94b0a860
Nikita Kanwar <kanwarnikita2025@gmail.com>
2025-03-24 09:58:35 +0530

    Create Usermodel and productmodel

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ import connectCloudinary from './config/cloudinary.js'
 const app = express()
 const port = process.env.PORT || 4000
 connectDB();
-connectCloudinary()
+connectCloudinary();
 
 // Middleware
 app.use(express.json())
