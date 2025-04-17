902efe1f0d743ce542e7f6977f320918df936671
Nikita Kanwar <kanwarnikita2025@gmail.com>
2025-04-17 20:22:42 +0530

    Complete

--- src/Pages/Collection.jsx
+++ src/Pages/Collection.jsx
@@ -130,7 +130,7 @@ const Collection = () => {
        <div className='flex justify-between text-base sm:text-2xl mb-4 '>
        <Title text1={'All'}  text2={'Collection'}/>
        <select onChange={(e) => setSortType(e.target.value)} className='border-2 border-gray-300 text-sm px-2'>
-        <option value="relavent">Sort by: Relavent</option>
+        <option value="relavent">Sort by: Relevant</option>
         <option value="low-high">Sort by: Low to High</option>
         <option value="high-low">Sort by: High to Low</option>
        </select>
